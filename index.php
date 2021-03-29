a42764fe291ccb4259b6a82df2405109471aef3c
Angel Dzib <angel_95pl@hotmail.com>
2021-03-29 18:44:29 -0500

    validation in data empty

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,54 +9,54 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Movie´s</title>
 </head>
+
 <body>
     <div class="container col-lg-5">
         <div class="card justify-content-md-center">
             <div class=" card-body"></div>
-                <form class="form-horizontal justify-content-center" id="formulario" method="POST" action="">
-                    <div class="form-group">
-                        <label for="nombre">Nombre</label>
-                        <input type="text" class="form-control" name="name" id="nombre" placeholder="last name / name">
-                        <span class="font-weight-light ml-1 require">Requerido</span>
-                    </div>
-                    <div class="form-group">
-                        <label for="Email">Email</label>
-                        <input type="text" class="form-control" name="email" id="Email" placeholder="ejemplo@ejemplo.com">
-                        <span class="font-weight-light ml-1 require">Requerido</span>
-                    </div>
-                    <div class="form-group">
-                        <label for="paquete" >Paquete</label>
-                        <select class="form-control" name="package" id="paquete">
-                            <option value="">Selecciona un paquete</option>
-                            <option value="a">Paquete A</option>
-                            <option value="b">Paquete B</option>
-                            <option value="c">Paquete C</option>
-                        </select>
-                        <span class="font-weight-light ml-1 require">Requerido</span>
-                    </div>
-                    <div class="form-group">
-                        <label for="Movie">Movie</label>
-                        <input type="text " name="movie" class="form-control" id="Movie" placeholder="Nombre de la pelicula">
-                        <span class="font-weight-light ml-1 require">Requerido</span>
-                    </div>
-                    <div class="form-group">
-                        <span>Precio : $100 MXN</span>
-                        <input type="hidden" name="price" class="form-control"  value="100">
-                        
-                    </div>
-                    
-                    <div class="form-group"> 
-                        <button type="submit" class="btn btn-primary btn-block" name="enviar">Pagar</button>
-                    </div>
-                    
-                </form>
-                <?php
-                 include("php/validar.php");
-                ?>
-            </div>
+            <form class="form-horizontal justify-content-center" id="formulario" method="POST" action="">
+                <div class="form-group">
+                    <label for="nombre">Nombre</label>
+                    <input type="text" class="form-control" name="name" id="nombre" placeholder="last name / name">
+                    <span class="font-weight-light ml-1 require">Requerido</span>
+                </div>
+                <div class="form-group">
+                    <label for="Email">Email</label>
+                    <input type="text" class="form-control" name="email" id="Email" placeholder="ejemplo@ejemplo.com">
+                    <span class="font-weight-light ml-1 require">Requerido</span>
+                </div>
+                <div class="form-group">
+                    <label for="paquete">Paquete</label>
+                    <select class="form-control" name="package" id="paquete">
+                        <option value="">Selecciona un paquete</option>
+                        <option value="a">Paquete A</option>
+                        <option value="b">Paquete B</option>
+                        <option value="c">Paquete C</option>
+                    </select>
+                    <span class="font-weight-light ml-1 require">Requerido</span>
+                </div>
+                <div class="form-group">
+                    <label for="Movie">Movie</label>
+                    <input type="text " name="movie" class="form-control" id="Movie" placeholder="Nombre de la pelicula">
+                    <span class="font-weight-light ml-1 require">Requerido</span>
+                </div>
+                <div class="form-group">
+                    <span>Precio : $100 MXN</span>
+                    <input type="hidden" name="price" class="form-control" value="100">
+
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-block" name="enviar">Pagar</button>
+                </div>
+
+            </form>
+
         </div>
     </div>
-    
+    </div>
+
     <script src="js/app.js"></script>
 </body>
+
 </html>
